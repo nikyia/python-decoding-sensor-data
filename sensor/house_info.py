0c709ff7f5a7b0ea89a825d34823eb5b2740e561
nikyia <106992582+nikyia@users.noreply.github.com>
2022-06-11 17:08:44 +0200

    Completed module

---
+++ sensor/house_info.py
@@ -0,0 +1,29 @@
+# Libraries
+from datetime import date, datetime
+from time import strftime
+
+# Crete class HouseInfo
+class HouseIinfo:
+    #Create class constructor with two arguments (self , class)
+    def __init__(self, data):
+        self.data = data
+    # Create method to select data by area (filter by rec area). Default value of rec_area = 0 zhich translates to all records
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record[field])
+
+        return field_data
+
+    def get_data_by_date(self, field, rec_date=datetime.now()):
+        field_data = []
+
+        for record in self.data:
+            if record['date'] == rec_date.strftime("%m/%d/%y"):
+                field_data.append(record[field])
+        
+        return field_data
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
+from house_info import HouseIinfo
+from datetime import date, datetime
 
 ##############################
 # Do not remove these two lines
@@ -16,6 +18,24 @@ print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
+
+# Get HouseInfo Data by Area with sensor_app
+house_info = HouseIinfo(data)
+
+test_area = 1
+
+recs = house_info.get_data_by_area('id', rec_area=test_area)
+
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
+# Get HouseInfo Data by Date with sensor_app
+
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+
+recs = house_info.get_data_by_date('id', rec_date=test_date)
+
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
 
 # Module 4 code here:
 
