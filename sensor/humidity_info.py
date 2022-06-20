64c6fcfb9fd420ddf3385b83db8876ef80a81f0a
nikyia <106992582+nikyia@users.noreply.github.com>
2022-06-20 12:38:22 +0200

    Completed module

---
+++ sensor/humidity_info.py
@@ -0,0 +1,20 @@
+from house_info import HouseInfo
+from datetime import date, datetime
+
+
+class HumidityDate(HouseInfo):
+    def _conver_data(self, data):
+        recs = []
+
+        for rec in data:
+            recs.append(float(rec)*100)
+
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("humidity", rec_area)
+        return self._conver_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("humidity", rec_date)
+        return self._conver_data(recs)
