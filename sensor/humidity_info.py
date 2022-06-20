6decdcc3454e78f206d5a8547b8e993484c3c87e
nikyia <106992582+nikyia@users.noreply.github.com>
2022-06-20 12:41:59 +0200

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 
 
 class HumidityData(HouseInfo):
-    def _conver_data(self, data):
+    def _convert_data(self, data):
         recs = []
 
         for rec in data:
@@ -13,8 +13,8 @@ class HumidityData(HouseInfo):
 
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("humidity", rec_area)
-        return self._conver_data(recs)
+        return self._convert_data(recs)
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("humidity", rec_date)
-        return self._conver_data(recs)
+        return self._convert_data(recs)
